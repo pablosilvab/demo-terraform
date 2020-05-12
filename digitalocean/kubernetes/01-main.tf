a562836fea5881bc1bf2e71ba19962eee3e51fa6
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-05-12 10:54:34 -0400

    update version k8s

--- digitalocean/kubernetes/01-main.tf
+++ digitalocean/kubernetes/01-main.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "pablodev" {
   name    = "pablodev"
   region  = "nyc1"
-  version = "1.16.6-do.2"
+  version = "1.17.5-do.0"
 
   node_pool {
     name       = "pablodev-nodes"
